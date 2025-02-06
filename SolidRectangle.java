d91eed988d864b3ca8253edce095093c6a1f9d0d
Aayansh Anand <95929174+aayanshanand@users.noreply.github.com>
2025-02-06 21:59:23 +0530

    Add files via upload
    This Java program prints a solid rectangle pattern of asterisks (*). The dimensions of the rectangle are 8 rows and 10 columns, defined by variables n and m. It uses nested loops:
    --> Outer loop (i) runs for n rows.
    -->Inner loop (j) prints m asterisks per row.
    -->A newline (System.out.println()) moves to the next row after printing m stars

---
+++ SolidRectangle.java
@@ -0,0 +1,13 @@
+public class SolidRectangle{
+    public static void main(String args[]) {
+        int n = 8 ;
+        int m = 10;
+            for(int i = 1; i <= n; i++){
+                for(int j = 1; j <= m; j++){
+                    System.out.print("*");
+                }
+                System.out.println("");
+            }
+    }
+
+}
